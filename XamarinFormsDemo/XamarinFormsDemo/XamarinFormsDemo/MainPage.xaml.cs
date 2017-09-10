29cda9895c9631d881a103cc47a6d65eb2bba226
Sarang K <k.sarang2009+demo@gmail.com>
2017-09-10 14:51:34 +0530

    Addition logic implemented

--- XamarinFormsDemo/XamarinFormsDemo/XamarinFormsDemo/MainPage.xaml.cs
+++ XamarinFormsDemo/XamarinFormsDemo/XamarinFormsDemo/MainPage.xaml.cs
@@ -12,6 +12,31 @@ namespace XamarinFormsDemo
         public MainPage()
         {
             InitializeComponent();
+            var btnCalculate = this.FindByName<Button>("btnCalculate");
+            btnCalculate.Clicked += btnCalculate_Clicked;
+        }
+
+        private void btnCalculate_Clicked(object sender, EventArgs e)
+        {
+            int param1, param2;
+            if (int.TryParse(txtParam1.Text, out param1) && int.TryParse(txtParam2.Text, out param2))
+            {
+                txtResult.Text = Addition(param1, param2).ToString();
+                return;
+            }
+
+            txtResult.Text = "Invalid input.";
+        }
+
+        private int Addition(int param1, int param2)
+        {
+            while (param2 != 0)
+            {
+                int carry = param1 & param2;
+                param1 = param1 ^ param2;
+                param2 = carry << 1;
+            }
+            return param1;
         }
     }
 }
