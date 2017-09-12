738f416a9427c657c10b8d8461024c9f4ae15864
Sarang K <k.sarang2009+demo@gmail.com>
2017-09-13 00:45:36 +0530

    Re-structured project in MVVM design pattern

--- XamarinFormsDemo/XamarinFormsDemo/XamarinFormsDemo/MainPage.xaml.cs
+++ XamarinFormsDemo/XamarinFormsDemo/XamarinFormsDemo/MainPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Xamarin.Forms;
+using XamarinFormsDemo.ViewModels;
 
 namespace XamarinFormsDemo
 {
@@ -14,42 +15,7 @@ namespace XamarinFormsDemo
         public MainPage()
         {
             InitializeComponent();
-            var btnCalculate = this.FindByName<Button>("btnCalculate");
-            btnCalculate.Clicked += btnCalculate_Clicked;
-        }
-
-        /// <summary>
-        /// Handles the Clicked event of the btnCalculate control.
-        /// </summary>
-        /// <param name="sender">The source of the event.</param>
-        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
-        private void btnCalculate_Clicked(object sender, EventArgs e)
-        {
-            int param1, param2;
-            if (int.TryParse(txtParam1.Text, out param1) && int.TryParse(txtParam2.Text, out param2))
-            {
-                txtResult.Text = Addition(param1, param2).ToString();
-                return;
-            }
-
-            txtResult.Text = "Invalid input.";
-        }
-
-        /// <summary>
-        /// Adds the specified param1 & param2.
-        /// </summary>
-        /// <param name="param1">The param1.</param>
-        /// <param name="param2">The param2.</param>
-        /// <returns></returns>
-        private int Addition(int param1, int param2)
-        {
-            while (param2 != 0)
-            {
-                int carry = param1 & param2;
-                param1 = param1 ^ param2;
-                param2 = carry << 1;
-            }
-            return param1;
-        }
+            BindingContext = new AdditionViewModel();
+        }        
     }
 }
---
+++ XamarinFormsDemo/XamarinFormsDemo/XamarinFormsDemo/ViewModels/AdditionViewModel.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinFormsDemo.ViewModels
+{
+    public class AdditionViewModel : INotifyPropertyChanged
+    {
+        public AdditionViewModel()
+        {
+            AdditionCommand = new Command(CalculateAddition);
+        }
+
+        string firstParameter = string.Empty;
+        string secondParameter = string.Empty;
+        int firstValue, secondValue;
+        string result;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName] string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public string FirstParameter
+        {
+            get { return firstParameter; }
+            set
+            {
+                firstParameter = value;
+            }
+        }
+
+        public string SecondParameter
+        {
+            get { return secondParameter; }
+            set
+            {
+                secondParameter = value;
+            }
+        }
+
+        public string Result
+        {
+            get { return result; }
+            set { result = value; }
+        }
+        
+        public Command AdditionCommand { get; set; }
+
+        void CalculateAddition()
+        {
+            try
+            {
+                if (int.TryParse(firstParameter, out firstValue) && int.TryParse(secondParameter, out secondValue))
+                {
+                    while (secondValue != 0)
+                    {
+                        int carry = firstValue & secondValue;
+                        firstValue = firstValue ^ secondValue;
+                        secondValue = carry << 1;
+                    }
+                    result = firstValue.ToString();
+                }
+                else
+                {
+                    result = "Invalid input.";
+                }                
+            }
+            catch (Exception)
+            {
+                Result = string.Empty;
+            }
+            finally
+            {
+                OnPropertyChanged(nameof(Result));
+            }
+        }
+    }
+}
